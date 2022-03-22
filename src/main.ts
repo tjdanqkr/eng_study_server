738b85e076197f3746600d2983981613e15dc692
tjdanqkr <tjdanqkr@gmail.com>
2022-03-22 11:00:45 +0900

    heroku

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(WINSTON_MODULE_PROVIDER));
-  await app.listen(3001);
+  await app.listen(process.env.PORT||3000);
 }
 bootstrap();
